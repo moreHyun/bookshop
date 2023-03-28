82bcdfa68c4f5fa4932e0b4a5f582e01a6489a3c
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-03-28 12:33:03 +0900

    230328 / 로그인 중간 수정 / 여다현

---
+++ src/main/java/com/greedy/bookshop/main/member/service/AuthenticationService.java
@@ -0,0 +1,15 @@
+package com.greedy.bookshop.main.member.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class AuthenticationService {
+
+  
+}
