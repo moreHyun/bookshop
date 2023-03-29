e12f46e30d38932d4c768cf092e1bad5d9437c4a
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-03-29 15:32:54 +0900

    230329/다현

--- src/main/java/com/greedy/bookshop/main/member/controller/MainController.java
+++ src/main/java/com/greedy/bookshop/main/member/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
     @GetMapping(value = {"/", "/main"})
     public String main() {
-        return "main/main";
+        return "/main";
     }
 
     @PostMapping(value="/")
