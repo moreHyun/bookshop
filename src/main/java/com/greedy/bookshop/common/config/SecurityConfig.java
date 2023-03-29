bf40b66cf5671a4bcd004d356e80cfee8d71159e
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2023-03-29 13:42:33 +0900

    230329/수정중/여다현

--- src/main/java/com/greedy/bookshop/common/config/SecurityConfig.java
+++ src/main/java/com/greedy/bookshop/common/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 /* hasRole 안에 전달하는 값은 "ROLE_" 가 자동으로 붙는다. */
                 .antMatchers(HttpMethod.POST, "/admin/**").hasRole("ADMIN")
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/member/register").authenticated()
+                .antMatchers(HttpMethod.POST, "/member/regist").authenticated()
                 /* 위에 서술 된 내용 외의 모든 요청은 허가함 (인증 되지 않은 사용자도 요청 가능) */
                 .anyRequest().permitAll()
                 .and()
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 /* 로그아웃 설정 */
                 .logout()
                 /* 로그아웃 주소 */
-                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .logoutRequestMatcher(new AntPathRequestMatcher("/main/main"))
                 /* 세션 만료 */
                 .invalidateHttpSession(true)
                 /* JSESSIONID 쿠키 삭제 */
