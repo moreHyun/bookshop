fffc6a6671a35da4e0523d9206ce8e78e450843c
kang chankyu <gangchanggyu9@gmail.com>
2023-03-29 17:33:13 +0900

    0329 main 안에 있는 css 변경 밑 디테일 from 태그 생성

--- src/main/resources/static/js/sales/bookDetails.js
+++ src/main/resources/static/js/sales/bookDetails.js
@@ -1,11 +1,3 @@
-window.onload = function ()
-{
-    let categoryList = `[[${categoryList}]]`;
 
-    console.log(categoryList);
-}
 
-document.getElementById("add-to-cart").addEventListener("click", function (){
-    let bookCode = document.getElementsByClassName("")
-})
 
