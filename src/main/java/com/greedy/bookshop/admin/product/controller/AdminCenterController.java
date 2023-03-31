0c6c68197348434bd42d9df6ccc22c5d3fb486d4
정기욱 <sktol96@naver.com>
2023-03-31 11:42:35 +0900

    20230331/admincenter 수정/정기욱

--- src/main/java/com/greedy/bookshop/admin/product/controller/AdminCenterController.java
+++ src/main/java/com/greedy/bookshop/admin/product/controller/AdminCenterController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/center")
+@RequestMapping("/admincenter")
 public class AdminCenterController {
 	
 	/* 공지사항 */
